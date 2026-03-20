fced26b63fce53aa4630b70a3c1b5dcd3dab10ef
plabonDS021 <sarkar.plabon2023@gmail.com>
2026-03-20 18:24:24 +0530

    loading data set

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+
+## load the data set
+
+df= pd.read_csv(r'C:\Users\PRADIPTA\Desktop\project\titanic data set\data\titanic\train.csv')
+df.head()
+df.describe()
+df.info()
